bcc195414bc4464953d45e94f8f5e36290d3957d
SeiyaWakabayashi <56055278+SeiyaWakabayashi@users.noreply.github.com>
2019-10-29 18:48:36 -0700

    Add files via upload

---
+++ Renban.java
@@ -0,0 +1,26 @@
+public class singleton {
+public static void main(String[] args){
+
+Renban r1 =Ranban.getInstanse();
+Renban r2 =Ranban.getInstanse();
+System.out.printin(r1.getNumber());
+System.out.printin(r2.getNumber());
+System.out.printin(r3.getNumber());
+
+}
+}
+class Ranban{
+ private static Ranban   
+
+
+
+}
+Renban r1 =Ranban.getInstanse();
+Renban r2 =Ranban.getInstanse();
+System.out.printin(r1.getNumber());
+System.out.printin(r2.getNumber());
+System.out.printin(r3.getNumber());
+
+
+
+
